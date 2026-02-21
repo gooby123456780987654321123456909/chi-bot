1aeca618d3c26092fd268fe90b36716bdc262686
chichannachi-chan <goobylolikulike@gmail.com>
2026-02-21 08:09:17 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,18 +12,18 @@ socket.on("message", function(data) {
    socket.send("fuck you, "+data.nick+"") 
   }
   if (data.msg == "chi!help") {
-    socket.send("chi!github chi!skibiditoilet chi!hello chi!jokes chi!tayo chi!channachi")  
+    socket.send("chi!github chi!skibiditoilet chi!hello chi!jokes chi!tayo chi!channachi chi!funfact")  
   }
   if (data.msg == "chi!tayo") {
     socket.send("wut?")
   }
   if (data.msg == "chi!jokes") {
     if (Math.random() <= 1/3) {
-      socket.send("HEY LITTLE TROLLYBOX USER! ITS TIME FOR A JOKE what do you call eggs? balls! hahahahah pls respond if you think its funny hahahah")
+      socket.send(" yeah a hentai "+data.nick+" wanna tells me a joke, what do you call eggs? balls! hahahahah pls respond if you think its funny hahahah")
     } else if (Math.random() <= 1/2) {
-      socket.send("HEY KIDS ITS TIME FOR A STUPID HORRIBLE THE JOKE THE CHINNACHI CHAN WROTE nothing hahahahahahaha")
+      socket.send("'haha look at this stupid "+data.nick+" telling chinnachi for a joke' fuck you, it isnt funny but i will do it, what did chichannachi-chan wrote? nothing hahahahahahaha")
     } else {
-      socket.send("HELLYO KIDDIES IF YOUR FIRED SAUCEAGE GETS HARD WHILE THEY SEE 2 WATERMELON THEY ARE FUCKING JERKING IT")
+      socket.send("wanna to see a joke? no? block me then that's your fucking problem but here we go. IF YOUR FIRED SAUCEAGE GETS HARD WHILE THEY SEE 2 WATERMELON THEY ARE FUCKING JERKING IT")
    }
   }
   if (data.msg.includes("67")) {
@@ -47,5 +47,8 @@ socket.on("message", function(data) {
   }
   if (data.msg == "chi!github") {
     socket.send("go to https://github.com/gooby123456780987654321123456909/chi-bot/tree/main if it is outdated then fahh")  
+  }
+  if (data.msg == "chi!funfact") {
+    socket.send("fun fact: people who plays free fire and thinks its better is also stupid")  
   }
 })
